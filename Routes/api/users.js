3fe20a81e69863a604b9b2b4a9d975fe56b4441c
Anton Veldanov <anton.veldanov@gmail.com>
2019-06-03 14:55:23 -0700

    server setup

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route   POST api/users
 //@desc    Register User
@@ -41,13 +43,15 @@ router.post('/', [
       })
 
       if (user) {
-        res.status(400).json({
+        return res.status(400).json({
           errors: [
             { msg: 'User already exists' }
           ]
         });
       }
 
+
+      // Get users gravatar
       const avatar = gravatar.url(email,
         {
           s: '200',
@@ -61,14 +65,33 @@ router.post('/', [
         avatar,
         password
       });
-      // Get users gravatar
+
 
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save()
 
-      // Return jsonwebtoken
+      // Return jsonwebtoken (in seconds)
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          // console.log(token);
+          res.json({ token });
+        })
 
 
-      res.send('User route')
+
+
+      // res.send('User registered')
 
     } catch (err) {
       console.log(err);
