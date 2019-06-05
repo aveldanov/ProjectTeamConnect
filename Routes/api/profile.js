83b1ac224da010fe1542112f6689c31849a33fb2
Anton Veldanov <anton.veldanov@gmail.com>
2019-06-04 22:14:12 -0700

    server setup

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator/check');
 
+//@route   GET api/profile/me
+//@desc    Get current user profile
+//@access  Private
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({
+        msg: '[api/profile] No profile found'
+      })
+    }
+
+    res.json(profile)
+  } catch (err) {
+
+    console.error(err.message);
+    res.status(500).send('[api/profile] Server Error')
+
+  }
+
+});
 
 
 //@route   GET api/profile
-//@desc    Test route
-//@access  Public
+//@desc    Create or update user profile
+//@access  Private
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.post('/', [auth,
+  [
+    check('status', '[api/profile] Status is required')
+      .not()
+      .isEmpty(),
+    check('skills', '[api/profile] Skills is required')
+  ]
+], (req, res) => {
 
+})
 
 module.exports = router;
